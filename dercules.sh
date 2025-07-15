432a3cd411f874a0cac559e7861d5eca44b45e47
Hoang H. Dang <hoang.dang.fv@renesas.com>
2025-07-15 17:31:44 +0900

    U

--- dercules.sh
+++ dercules.sh
@@ -1,3 +1,3 @@
-cp -f configs/.tmux.conf .tmux.conf
-cp -f configs/git-tools.sh git-tools.sh
+cp -f configs/.tmux.conf $HOME/.tmux.conf
+cp -f configs/git-tools.sh $HOME/git-tools.sh
 echo "source \$HOME/git-tools.sh" >> $HOME/.bashrc
