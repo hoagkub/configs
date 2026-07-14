952f1fc9b39a429720c851aa773421c9608cb88e
Hoang H. Dang <34186687+hoagkub@users.noreply.github.com>
2026-07-14 13:19:36 +0700

    Update dercules.sh

--- dercules.sh
+++ dercules.sh
@@ -1,3 +1,9 @@
 cp -f configs/.tmux.conf $HOME/.tmux.conf
 cp -f configs/git-tools.sh $HOME/git-tools.sh
 echo "source \$HOME/git-tools.sh" >> $HOME/.bashrc
+
+# lf
+wget https://github.com/gokcehan/lf/releases/latest/download/lf-linux-amd64.tar.gz
+tar -xzf lf-linux-amd64.tar.gz
+wget https://raw.githubusercontent.com/gokcehan/lf/master/etc/lfcd.sh
+echo "source \$HOME/lfcd.sh" >> $HOME/.bashrc
