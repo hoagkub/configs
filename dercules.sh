0756e2905bf7940661609e38b1f4dbc5b1b353a4
Hoang H. Dang <34186687+hoagkub@users.noreply.github.com>
2026-07-14 13:41:51 +0700

    Update dercules.sh

--- dercules.sh
+++ dercules.sh
@@ -1,9 +1,15 @@
-cp -f configs/.tmux.conf $HOME/.tmux.conf
-cp -f configs/git-tools.sh $HOME/git-tools.sh
-echo "source \$HOME/git-tools.sh" >> $HOME/.bashrc
-
 # lf
 wget https://github.com/gokcehan/lf/releases/latest/download/lf-linux-amd64.tar.gz
 tar -xzf lf-linux-amd64.tar.gz
+mkdir -p ~/.local/bin
+mv ~/lf ~/.local/bin/
+echo 'export PATH="$HOME/.local/bin:$PATH"' >> ~/.bashrc
 wget https://raw.githubusercontent.com/gokcehan/lf/master/etc/lfcd.sh
 echo "source \$HOME/lfcd.sh" >> $HOME/.bashrc
+
+# tmux
+cp -f configs/.tmux.conf $HOME/.tmux.conf
+
+# git
+cp -f configs/git-tools.sh $HOME/git-tools.sh
+echo "source \$HOME/git-tools.sh" >> $HOME/.bashrc
